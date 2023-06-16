51a87c796c2945f5c282b8f322bb5e79c4b4316f
Galih Agus Saputra <agussaputragalih@gmail.com>
2023-06-16 21:00:32 +0700

    First Porto

---
+++ bootstrap/ssr/assets/index-01afdf48.mjs
@@ -0,0 +1,47 @@
+import { withCtx, createVNode, useSSRContext } from "vue";
+import { ssrRenderComponent } from "vue/server-renderer";
+import { _ as _sfc_main$1 } from "./AuthenticatedLayout-2d24cec4.mjs";
+import "@inertiajs/vue3";
+import "./ApplicationLogo-6bd635f3.mjs";
+const _sfc_main = {
+  __name: "index",
+  __ssrInlineRender: true,
+  setup(__props) {
+    return (_ctx, _push, _parent, _attrs) => {
+      _push(`<!--[--><head title="Skills Index"></head>`);
+      _push(ssrRenderComponent(_sfc_main$1, null, {
+        header: withCtx((_, _push2, _parent2, _scopeId) => {
+          if (_push2) {
+            _push2(`<h2 class="font-semibold text-xl text-gray-800 leading-tight"${_scopeId}> Skills </h2>`);
+          } else {
+            return [
+              createVNode("h2", { class: "font-semibold text-xl text-gray-800 leading-tight" }, " Skills ")
+            ];
+          }
+        }),
+        default: withCtx((_, _push2, _parent2, _scopeId) => {
+          if (_push2) {
+            _push2(`<div class="py-12"${_scopeId}><div class="max-w-7xl mx-auto sm:px-6 lg:px-8"${_scopeId}> Table </div></div>`);
+          } else {
+            return [
+              createVNode("div", { class: "py-12" }, [
+                createVNode("div", { class: "max-w-7xl mx-auto sm:px-6 lg:px-8" }, " Table ")
+              ])
+            ];
+          }
+        }),
+        _: 1
+      }, _parent));
+      _push(`<!--]-->`);
+    };
+  }
+};
+const _sfc_setup = _sfc_main.setup;
+_sfc_main.setup = (props, ctx) => {
+  const ssrContext = useSSRContext();
+  (ssrContext.modules || (ssrContext.modules = /* @__PURE__ */ new Set())).add("resources/js/Pages/Skills/index.vue");
+  return _sfc_setup ? _sfc_setup(props, ctx) : void 0;
+};
+export {
+  _sfc_main as default
+};
